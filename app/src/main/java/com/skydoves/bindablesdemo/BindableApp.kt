50e32a2a205d0ea9fbdbb202255204f44db40c49
skydoves <skydoves@naver.com>
2021-02-19 19:15:37 +0900

    Implement BindingInitProvider for binding Bindable properties on app start

--- app/src/main/java/com/skydoves/bindablesdemo/BindableApp.kt
+++ app/src/main/java/com/skydoves/bindablesdemo/BindableApp.kt
@@ -17,15 +17,7 @@
 package com.skydoves.bindablesdemo
 
 import android.app.Application
-import com.skydoves.bindables.BindingManager
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class BindableApp : Application() {
-
-  override fun onCreate() {
-    super.onCreate()
-
-    BindingManager.bind(BR::class)
-  }
-}
+class BindableApp : Application()
