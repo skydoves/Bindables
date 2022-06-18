f177fc38ce507e2444c1bf9127c60f38b9835e80
skydoves <skydoves@naver.com>
2022-06-18 16:37:01 +0900

    Update min sdk to 21

--- buildSrc/src/main/kotlin/com/skydoves/bindables/Configuration.kt
+++ buildSrc/src/main/kotlin/com/skydoves/bindables/Configuration.kt
@@ -19,7 +19,7 @@ package com.skydoves.bindables
 object Configuration {
   const val compileSdk = 32
   const val targetSdk = 32
-  const val minSdk = 16
+  const val minSdk = 21
   const val majorVersion = 1
   const val minorVersion = 1
   const val patchVersion = 0
